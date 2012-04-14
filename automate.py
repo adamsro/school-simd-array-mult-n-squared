a9e53e5f974a42c6494a9289ab4cb70e5450da16
rma.kona <rma.kona@gmail.com>
2012-04-14 10:48:16 -0700

    update automate.py for new name

--- automate.py
+++ automate.py
@@ -6,8 +6,8 @@ def main():
 	step = [1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000, 256000,
 			512000, 1024000, 2048000, 4096000, 8192000]
 	for n in step:
-		os.system("g++  -DNUM=%d  -o arraymult  arraymult.cpp simd.o  -lm -fopenmp" % (n))
-		os.system("./arraymult")
+		os.system("g++  -DNUM=%d  -o autocorrelation autocorrelation.cpp simd.o  -lm -fopenmp" % (n))
+		os.system("./autocorrelation")
 
 if __name__ == "__main__":
 	main()
